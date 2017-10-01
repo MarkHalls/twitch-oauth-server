1683c707bf78479dfabc06d0ee66c3af3e811f4f
Mark Halls <firesetterdev@gmail.com>
2017-10-01 15:38:31 -0700

    Fix indentation and alignment

--- src/db/migrations/001-initial-schema.sql
+++ src/db/migrations/001-initial-schema.sql
@@ -1,17 +1,16 @@
 -- Up
 CREATE TABLE config (
-	id								INTEGER			PRIMARY KEY,
-	bot_name					TEXT				COLLATE NOCASE,
-	twitch_client_id	TEXT				COLLATE BINARY,
-	twitch_bot_token	TEXT				COLLATE BINARY,
-	discord_bot_token	TEXT				COLLATE BINARY,
-
+  id                INTEGER PRIMARY KEY,
+  bot_name          TEXT    COLLATE NOCASE,
+  twitch_client_id  TEXT    COLLATE BINARY,
+  twitch_bot_token  TEXT    COLLATE BINARY,
+  discord_bot_token TEXT    COLLATE BINARY,
 )
 
 CREATE TABLE users (
-	id								INTEGER			PRIMARY KEY,
-	twitch_name				TEXT				COLLATE NOCASE,
-	twitch_oauth			TEXT				COLLATE NOCASE
+  id                INTEGER PRIMARY KEY,
+  twitch_name       TEXT    COLLATE NOCASE,
+  twitch_oauth      TEXT    COLLATE NOCASE
 );
 
 -- Down
