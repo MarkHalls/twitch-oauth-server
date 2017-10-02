06934090dc38e17520d1bb3c43f5b0ed66e64d5f
Mark Halls <firesetterdev@gmail.com>
2017-10-01 17:47:03 -0700

    Generate twitch auth url with random state

--- src/common/oauth/oauth.ts
+++ src/common/oauth/oauth.ts
@@ -1,10 +1,47 @@
-import api from "twitch-api-v5";
+import { stringify } from "querystring";
+import crypto from "crypto";
+import { promisify } from "util";
 
-api.clientID = "clientID";
-const data = {
-  clientSecret: secret,
-  redirectURI: "http://oauth.alwaysbecrafting.stream",
-  code
-};
+type Scope =
+  | "channel_check_subscription"
+  | "channel_commercial"
+  | "channel_editor"
+  | "channel_feed_edit"
+  | "channel_feed_read"
+  | "channel_read"
+  | "channel_stream"
+  | "channel_subscriptions"
+  | "chat_login"
+  | "collections_edit"
+  | "communities_edit"
+  | "communities_moderate"
+  | "openid"
+  | "user_blocks_edit"
+  | "user_blocks_read"
+  | "user_follows_edit"
+  | "user_read"
+  | "user_subscriptions"
+  | "viewing_activity_read";
 
-api.auth.getAccessToken(data, () => {});
+interface TokenOptions {
+  redirect_uri: string;
+  scope: Scope[];
+  state: string;
+}
+const getAuthorizeUrl = (client_id: string, opts: TokenOptions): string => {
+  const query = stringify({
+    client_id,
+    redirect_uri: opts.redirect_uri,
+    scope: opts.scope.join(" "),
+    response_type: "code",
+    state: opts.state,
+  })
+  return `https://api.twitch.tv/kraken/oauth2/authorize?${query}`;
+}
+
+const randomBytes = promisify(crypto.randomBytes);
+const generateStateToken = () => (
+  randomBytes(16).then(buf => buf.toString("hex"))
+);
+
+export { getAuthorizeUrl, generateStateToken };
