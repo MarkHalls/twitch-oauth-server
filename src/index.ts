fd6f555670fecc7815b49a625eb897e5f66c6d08
Mark Halls <firesetterdev@gmail.com>
2017-10-01 18:54:13 -0700

    Add express, setup '/authorize' route

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import sqlite from "sqlite";
+import express from "express";
 
 import { init } from "./db/database";
+import { TokenOptions, getAuthorizeUrl, generateStateToken} from "./common/oauth";
+import auth from "./common/config/auth.json";
+
+const app = express();
 
 const db = sqlite.open(`${__dirname}/common/db/sqlite.db`);
+
 db.then(database => init(database, "last")).then();
 
-if (process.getuid !== 0) {
-  console.err("Error: You must be root");
-}
-try {
-  process.setuid("myuser");
-  console.log("UID: ", process.getuid());
-} catch (err) {
-  console.error(err);
-}
+app.get("/authorize", async () => {
+  const options: TokenOptions = {
+    redirect_uri: auth.redirect_uri,
+    scope: auth.scope,
+    state: await generateStateToken() 
+  };
+  getAuthorizeUrl(auth.client_id, options);
+});
+
+app.listen(3000);
